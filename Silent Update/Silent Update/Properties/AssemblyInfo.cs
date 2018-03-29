5b86ef80b014b0aa9a480836c963e10db011cdea
Cam Thang Duong <thangduong1982@gmail.com>
2018-03-29 11:30:26 -0400

    Add icon navigation
    Add icon navigation

--- Silent Update/Silent Update/Properties/AssemblyInfo.cs
+++ Silent Update/Silent Update/Properties/AssemblyInfo.cs
@@ -7,19 +7,19 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Silent Update")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Silent Update Wizard")]
+[assembly: AssemblyDescription("Audit Silent Update Wizard")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Silent Update")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("CaseWare International Inc.")]
+[assembly: AssemblyProduct("Silent Update Wizard")]
+[assembly: AssemblyCopyright("CaseWare International Inc. ©  2018")]
+[assembly: AssemblyTrademark("CaseWare International Inc.")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 //In order to begin building localizable applications, set
 //<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
