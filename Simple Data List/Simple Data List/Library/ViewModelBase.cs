fa7afb7a9e90bf4df1e2a5f429c8861b0e3bf5b2
camthangduong <cam.thang.duong@gmail.com>
2018-05-15 22:44:24 -0400

    Testing the binging with ListViewCollection
    Considering the Observable Collection

---
+++ Simple Data List/Simple Data List/Library/ViewModelBase.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace Simple_Data_List.Library
+{
+    /// <summary>
+    /// Base view model abstract class, all other view model will be based 
+    /// </summary>
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string prop)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(prop));
+        }
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            this.PropertyChanged?.Invoke(this, e);
+        }
+    }
+}
---
+++ Simple Data List/Simple Data List/ViewModels/PreStatusViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Simple_Data_List.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Data_List.ViewModels
+{
+    public class PreStatusViewModel : ViewModelBase
+    {
+    }
+}
